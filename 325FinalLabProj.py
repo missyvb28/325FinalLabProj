dc595f2c3165ef9fff46a25609a6cfdcf3c715f1
missyvb28 <missyvb28@gmail.com>
2018-12-04 20:16:16 -0500

    type here what was changed
    (stuff)

--- 325FinalLabProj.py
+++ 325FinalLabProj.py
@@ -8,91 +8,49 @@
 print("Hello World")
 #helpful: how to do object detection with opencv [live] on youtube by a siraj
 
-#import statements
+
+from collections import deque
+from imutils.video import VideoStream
 import numpy as np
+import argparse
 import cv2
 import imutils
-import argparse
-from imutils.video import VideoStream
-
-#video port
-cap = cv2.VideoCapture(0)
-
-#RGB values
-# dark orange [16,100,68.6]
-# light orange [18,75.1,96.1]
-DarkOrng = np.array(min_org, cv2.COLOR_RGB2HSV)
-LightOrng = cv2.cvtColor(max_org, cv2.COLOR_RGB2HSV)
-
+ 
+#upper/lower bounds
+#lower_color= (29, 86, 6)
+#upper_color = (64, 255, 255)
+upper_color = (174, 223, 255)
+lower_color = (125, 106, 91)
+ 
+#argument parse
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", help="path to the (optional) video file")
+args = vars(ap.parse_args())
+ 
+if not args.get("video", False):
+  vs = VideoStream(src=0).start()
+else:
+  vs = cv2.VideoCapture(args["video"])
+ 
+#Keep looking until quiting
 while True:
-	ret, frame = cap.read()
-	#name of window and making window
-	#cv2.imshow('frame',frame)
-	
-	#get the current frame from the camera
-	frame = vs.read()
+  #Grabs the current frame that is being used
+  frame = vs.read()
+  
+  #If the frame is not successfuly read, break from the loop
+  if frame is None:
+    break
+  
+  #Resizes frame to desired width
+  frame = imutils.resize(frame, width=1600)
+  
+  #Blurs frame to remove noise: size of the kernel is 11,11 and standard deviation
+  blurred_frame = cv2.GaussianBlur(frame, (11,11), 0)
+  
+  #Convert frame to HSV
+  hsv_frame = cv2.cvtColor(blurred_frame, cv2.COLOR_BGR2HSV)
+  
+  #Mask for the upper/lower color bounds
+  mask = cv2.inRange(hsv_frame, lower_color, upper_color)
+  
 
-	#handling the video stream
-	if args.get("Video", False):
-		frame = frame[1] #if there's no video feed
-	else:
-		frame = frame #if there is video feed
-
-	#if the frame isn't successfully read, then quit
-	if frame is None:
-		break
-
-	#resize the frame
-	frame.imutils.resize(frame, width=600)
-
-	#blur the frame, smooth out color variations
-	blurred_frame = cv2.GaussianBlur(frame, (11,11), 0)
-
-	#convert frame to HSV (Hue Saturation Value)
-	hsv_frame = cv2.cvtColor(blurred_frame, cv2.COLOR_BGR2HSV)
-
-	#mask the frame to find the puck w/in the upper/lower color bounds
-	mask = cv2.inRange(hsv, DarkOrng, LightOrng)
-
-	#erode everything else
-	mask = cv2.erode(mask, None, iterations=2)
-
-	#dilation
-	mask = cv2.dilate(mask, None, iterations=2)
-
-	contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-	#validates OpenCV giving us contours
-	if imutils.is_cv2():
-		contours = contours[0]
-	else:
-		contours = contours[1]
-
-	#center of the object
-	center = None
-
-	#find an existing contour
-	if len(contours) > 0:
-		centroid = max(contours, key = cv2.contourArea)
-		((x, y), radius) = cv2.minEnclosingCircle(centroid)
-		moment = cv2.moments(centroid)
-
-		#find the coordinates of the center
-		center = (int(moment["m10"] / moment["m00"]), int(moment["m01"] / moment["m00"]))
-
-	#draw a rectangle around the object being tracked
-	if radius > 10:
-		#draw yellow rectangle
-		cv2.rectangle(frame, (int(x), int(y)), int(radius), (0,0,255), thickness = 3)
-		#draw a purple line following the object
-		cv2.circle(frame, center, 5, (204, 0, 102), -1)
-
-	#display current frame on screen
-	cv2.imshow("frame", frame)
-
-	#to quit window, press q
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
-
-cap.release()
-cv2.destroyAllWindows()
