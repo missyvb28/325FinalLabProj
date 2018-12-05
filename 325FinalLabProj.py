1268f62318b7e6e0261ed6012e097439bc4580d0
missyvb28 <missyvb28@gmail.com>
2018-12-04 20:37:31 -0500

    Code for object tracking.  Tracked a fuschia colored object.  Melissa's fuschia chapstick used for demonstration.
    Old code being replaced was the color tolerance generator.

--- 325FinalLabProj.py
+++ 325FinalLabProj.py
@@ -5,10 +5,6 @@
 #Isaac Rai
 #Daniel Wartella
 
-print("Hello World")
-#helpful: how to do object detection with opencv [live] on youtube by a siraj
-
-
 from collections import deque
 from imutils.video import VideoStream
 import numpy as np
@@ -42,7 +38,7 @@ while True:
     break
   
   #Resizes frame to desired width
-  frame = imutils.resize(frame, width=1600)
+  frame = imutils.resize(frame, width=600)
   
   #Blurs frame to remove noise: size of the kernel is 11,11 and standard deviation
   blurred_frame = cv2.GaussianBlur(frame, (11,11), 0)
@@ -53,4 +49,59 @@ while True:
   #Mask for the upper/lower color bounds
   mask = cv2.inRange(hsv_frame, lower_color, upper_color)
   
-
+  #Erosion
+  mask = cv2.erode(mask, None, iterations=2)
+  
+  #Dilation
+  mask = cv2.dilate(mask, None, iterations=2)
+ 
+  #array of contours
+  #takes in masked image, takes out child contours, removes redundant points, and compresses the contours
+  contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+  
+  #Validates OpenCV giving contours
+  if imutils.is_cv2():
+    contours = contours[0]
+  else:
+    contours = contours[1]
+ 
+  #if statement to find an existing contour
+  if len(contours) > 0:
+    #Calculates the centroid of the max contour
+    centroid = max(contours, key=cv2.contourArea)
+    
+    #Finds the x, y, and radius of the max contour
+    ((x, y), radius) = cv2.minEnclosingCircle(centroid)
+    
+    #Output x and y coordinates
+    #print(int(x), int(y))
+    
+ 	#Process to draw rectangle around tracked object
+    if radius > 5:
+      #Draws a yellow rectangle around tracked object
+      cv2.rectangle(frame, (int(x)-int(radius), int(y)-int(radius)),(int(x)+int(radius), int(y)+int(radius)), (0, 255, 255), 6)
+      #Outputs text "ACTIVE"
+      cv2.putText(frame, 'TRACKING SYSTEM: ACTIVE', (10,650), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 3)
+    else:
+        #Outputs text "INACTIVE" since the radius of the contour is too small to read
+      cv2.putText(frame, 'TRACKING SYSTEM: INACTIVE', (10,650), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 3)
+  else:
+    #Outpus text "INACTIVE" since no contours are read
+    cv2.putText(frame, 'TRACKING SYSTEM: INACTIVE', (10,650), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 3)
+ 
+  #Shows the current frame to the screen
+  cv2.imshow("Live Video", frame)
+ 
+  #Shows the HSV frame
+  #cv2.imshow("HSV", mask)
+ 
+  #If q is pressed, the video stream stops
+  if cv2.waitKey(1) == ord("q"):
+    break
+ 
+if not args.get("video", False):
+  vs.stop()
+else:
+  vs.release()
+ 
+cv2.destroyAllWindows()
